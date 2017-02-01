ee6fe3e5474fa1c7ec18464a275b9a6b24565ca8
Mark Stevens <steven25@wwu.edu>
2017-02-01 08:38:31 -0800

    fix code to display link to users profile page

--- templates/node--program.tpl.php
+++ templates/node--program.tpl.php
@@ -19,23 +19,16 @@
  *
  */
 
-// retrieves relevant part of the dom
-$dom = new DomDocument();
-$dom->loadHTML(render($content["field_instructor"]));
-
-// retrieves link to staff profile page
-$item = $dom->getElementsByTagName('a')->item(0);
-$href = $item->getAttribute('href');
-$anchorText = $item->nodeValue;
+$CASName = $content["og_group_ref"]["#object"]->field_instructor["und"][0]["entity"]->name;
 
 // retrieves first and last name from view
-$result = views_get_view_result('instructor_search_by_name', 'entityreference_1', $anchorText);
+$result = views_get_view_result('instructor_search_by_name', 'entityreference_3', $CASName);
 $firstName = $result[0]->_field_data["uid"]["entity"]->field_preferred_name["und"][0]["value"];
 $lastName = $result[0]->_field_data["uid"]["entity"]->field_last_name["und"][0]["value"];
 $fullName = $firstName . " " . $lastName;
 
 // builds link to staff profile page using first and last name
-$link = "<a href=" .  $href .  ">$fullName</a>";
+$link = "<a href=" . base_path() . "users/" .  $CASName .  ">$fullName</a>";
 ?>
 
 <article class="node-<?php print $node->nid; ?> <?php print $classes; ?> clearfix"<?php print $attributes; ?>>
